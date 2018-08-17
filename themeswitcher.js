4941a6c529ddb69278a0504cb4406a8c91303da7
Marcin Mikołajczak <me@m4sk.in>
2018-08-17 12:30:53 +0000

    just a bit of contrast in dark theme in results

--- themeswitcher.js
+++ themeswitcher.js
@@ -2,7 +2,7 @@ function set_dark() {
     localStorage.setItem("dark-theme", 'true');
     let sheet = document.createElement('style');
     sheet.id = 'dark';
-    sheet.innerHTML = "body {background-color: #222; border-color: #111} p.question {color: #ccc; background-color: #111} p {color: #bbb} h1 {color: #ddd} h2 {color: #ccc} #values p {color: #fff} a:link {color: #fff} a:visited {color: #aaf} .small_button_off {color: #ddd; border: 2px solid #ddd; background-color: #222}"
+    sheet.innerHTML = "body {background-color: #181818; border-color: #111} p.question {color: #ccc; background-color: #111} p {color: #bbb} h1 {color: #ddd} h2 {color: #ccc} #values p {color: #fff} a:link {color: #fff} a:visited {color: #aaf} .small_button_off {color: #ddd; border: 2px solid #ddd; background-color: #222}"
     document.body.appendChild(sheet);
 }
 
