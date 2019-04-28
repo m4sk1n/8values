56df4c0b98e1f5be74cfb6e3682ff5a7ef988617
marcin mikołajczak <me@m4sk.in>
2019-04-28 17:05:14 +0200

    update, go back to github
    Signed-off-by: marcin mikołajczak <me@m4sk.in>

---
+++ docs/ideologies.js
@@ -0,0 +1,313 @@
+ideologies = {
+    "ideo1": {
+        "econ": 100,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 90
+    },
+    "ideo2": {
+        "econ": 100,
+        "dipl": 80,
+        "govt": 80,
+        "scty": 80
+    },
+    "ideo3": {
+        "econ": 100,
+        "dipl": 100,
+        "govt": 60,
+        "scty": 80
+    },
+    "ideo4": {
+        "econ": 100,
+        "dipl": 80,
+        "govt": 30,
+        "scty": 80
+    },
+    "ideo5": {
+        "econ": 100,
+        "dipl": 30,
+        "govt": 30,
+        "scty": 80
+    },
+    "ideo6": {
+        "econ": 100,
+        "dipl": 40,
+        "govt": 20,
+        "scty": 70
+    },
+    "ideo7": {
+        "econ": 100,
+        "dipl": 20,
+        "govt": 0,
+        "scty": 60
+    },
+    "ideo8": {
+        "econ": 100,
+        "dipl": 50,
+        "govt": 30,
+        "scty": 30
+    },
+    "ideo9": {
+        "econ": 80,
+        "dipl": 30,
+        "govt": 30,
+        "scty": 70
+    },
+    "ideo10": {
+        "econ": 80,
+        "dipl": 50,
+        "govt": 30,
+        "scty": 20
+    },
+    "ideo11": {
+        "econ": 80,
+        "dipl": 50,
+        "govt": 70,
+        "scty": 20
+    },
+    "ideo12": {
+        "econ": 80,
+        "dipl": 50,
+        "govt": 50,
+        "scty": 80
+    },
+    "ideo13": {
+        "econ": 80,
+        "dipl": 20,
+        "govt": 50,
+        "scty": 70
+    },
+    "ideo14": {
+        "econ": 80,
+        "dipl": 80,
+        "govt": 80,
+        "scty": 80
+    },
+    "ideo15": {
+        "econ": 80,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 80
+    },
+    "ideo16": {
+        "econ": 60,
+        "dipl": 40,
+        "govt": 30,
+        "scty": 70
+    },
+    "ideo17": {
+        "econ": 60,
+        "dipl": 40,
+        "govt": 30,
+        "scty": 20
+    },
+    "ideo18": {
+        "econ": 60,
+        "dipl": 50,
+        "govt": 50,
+        "scty": 20
+    },
+    "ideo19": {
+        "econ": 60,
+        "dipl": 60,
+        "govt": 60,
+        "scty": 80
+    },
+    "ideo20": {
+        "econ": 60,
+        "dipl": 60,
+        "govt": 40,
+        "scty": 30
+    },
+    "ideo21": {
+        "econ": 60,
+        "dipl": 70,
+        "govt": 40,
+        "scty": 80
+    },
+    "ideo22": {
+        "econ": 60,
+        "dipl": 80,
+        "govt": 60,
+        "scty": 100
+    },
+    "ideo23": {
+        "econ": 60,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 70
+    },
+    "ideo24": {
+        "econ": 50,
+        "dipl": 10,
+        "govt": 0,
+        "scty": 50
+    },
+    "ideo25": {
+        "econ": 50,
+        "dipl": 90,
+        "govt": 0,
+        "scty": 50
+    },
+    "ideo26": {
+        "econ": 60,
+        "dipl": 60,
+        "govt": 20,
+        "scty": 80
+    },
+    "ideo27": {
+        "econ": 50,
+        "dipl": 50,
+        "govt": 50,
+        "scty": 50
+    },
+    "ideo28": {
+        "econ": 50,
+        "dipl": 60,
+        "govt": 60,
+        "scty": 60
+    },
+    "ideo29": {
+        "econ": 50,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 20
+    },
+    "ideo30": {
+        "econ": 40,
+        "dipl": 30,
+        "govt": 30,
+        "scty": 30
+    },
+    "ideo31": {
+        "econ": 40,
+        "dipl": 40,
+        "govt": 50,
+        "scty": 30
+    },
+    "ideo32": {
+        "econ": 40,
+        "dipl": 40,
+        "govt": 40,
+        "scty": 10
+    },
+    "ideo33": {
+        "econ": 60,
+        "dipl": 70,
+        "govt": 80,
+        "scty": 70
+    },
+    "ideo34": {
+        "econ": 40,
+        "dipl": 60,
+        "govt": 80,
+        "scty": 60
+    },
+    "ideo35": {
+        "econ": 40,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 50
+    },
+    "ideo36": {
+        "econ": 50,
+        "dipl": 0,
+        "govt": 0,
+        "scty": 5
+    },
+    "ideo37": {
+        "econ": 50,
+        "dipl": 20,
+        "govt": 20,
+        "scty": 50
+    },
+    "ideo38": {
+        "econ": 40,
+        "dipl": 10,
+        "govt": 10,
+        "scty": 20
+    },
+    "ideo39": {
+        "econ": 10,
+        "dipl": 10,
+        "govt": 10,
+        "scty": 20
+    },
+    "ideo40": {
+        "econ": 30,
+        "dipl": 40,
+        "govt": 40,
+        "scty": 20
+    },
+    "ideo41": {
+        "econ": 30,
+        "dipl": 30,
+        "govt": 50,
+        "scty": 60
+    },
+    "ideo42": {
+        "econ": 30,
+        "dipl": 60,
+        "govt": 60,
+        "scty": 80
+    },
+    "ideo43": {
+        "econ": 20,
+        "dipl": 30,
+        "govt": 20,
+        "scty": 40
+    },
+    "ideo44": {
+        "econ": 20,
+        "dipl": 50,
+        "govt": 30,
+        "scty": 50
+    },
+    "ideo45": {
+        "econ": 20,
+        "dipl": 20,
+        "govt": 40,
+        "scty": 20
+    },
+    "ideo46": {
+        "econ": 20,
+        "dipl": 30,
+        "govt": 30,
+        "scty": 5
+    },
+    "ideo47": {
+        "econ": 20,
+        "dipl": 50,
+        "govt": 80,
+        "scty": 60
+    },
+    "ideo48": {
+        "econ": 20,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 50
+    },
+    "ideo49": {
+        "econ": 10,
+        "dipl": 50,
+        "govt": 90,
+    },
+    "ideo50": {
+        "econ": 0,
+        "dipl": 20,
+        "govt": 0,
+        "scty": 50
+    },
+    "ideo51": {
+        "econ": 0,
+        "dipl": 40,
+        "govt": 50,
+        "scty": 50
+    },
+    "ideo52": {
+        "econ": 0,
+        "dipl": 50,
+        "govt": 100,
+        "scty": 50
+    }
+};
